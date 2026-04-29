0bb19b7a0dfa856947221c3b2343be72c32998c8
Hasibul Hasan <hasibulh798@gmail.com>
2026-04-29 11:06:35 +0600

    some change in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
     ],
   },
 
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000"}/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
