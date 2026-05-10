d83a2c2a8958fd8edf963bdeea985656c9e5d400
Hasibul Hasan <hasibulh798@gmail.com>
2026-05-10 23:36:38 +0600

    fix: modify in auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 
 export const authClient = createAuthClient({
-  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/auth`,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth`,
   fetchOptions: {
     credentials: "include",
   },
