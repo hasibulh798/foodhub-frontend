3720904062ade70141fa82778c462a45626bcc97
Hasibul Hasan <hasibulh798@gmail.com>
2026-05-13 23:18:53 +0600

    rewrite auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000",
-  // baseURL: '/api/auth',
+  baseURL: process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000",
   fetchOptions: {
     credentials: "include",
   },
